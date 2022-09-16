e59f819036b58cb8cc94c414873381362de6839c
Amanghvxh <90203726+Amanghvxh@users.noreply.github.com>
2022-09-16 13:27:54 +0000

    Add files via upload

---
+++ clockJavaScript/README.md
@@ -0,0 +1,5 @@
+# Second project from javascript30dayschellenge
+
+This code is not at all refactored and there are so many things that can be done effeciently!
+
+Hosted with netlify(link-+' ') ---> {Desktop version only}
---
+++ clockJavaScript/script.js
@@ -0,0 +1,155 @@
+const secondsHand = document.querySelector('.seconds_hand')
+const minutesHand = document.querySelector('.minutes_hand')
+const hoursHand   = document.querySelector('.hours_hand')
+const AmPm = document.querySelector('.span_amPm')
+const location2 = navigator.geolocation
+const body = document.querySelector('body')
+const hands = document.querySelectorAll('.hand')
+const img = document.querySelectorAll('img')
+const triangle = document.querySelectorAll('.triangle')
+const darkToggle = document.querySelector('.dark_mode_toggle')
+const darkToggleButton = document.querySelector('.toggle_button')
+const darkContainer = document.querySelector('.alarm_container')
+const darkSubmitButton = document.querySelector('.submit')
+const darkInputs = document.querySelectorAll('.alarm_inputs')
+const alarmText = document.querySelector('.alarm_text')
+let hours,seconds,minutes,aMinute,aHour,aSecond,timeSec,intervalId
+const snoozeButton = document.querySelector('.snooze')
+const audio = document.querySelector('.audio')
+const stopButton = document.querySelector('.stop_alarm')
+const alarmContainer = document.querySelector('.alarm_time')
+//const getPosition = function(position){
+    //const lat = position.coords.latitude
+  //  const long = position.coords.longitude
+   // return [lat,long]
+
+//}
+
+const darkMode = function(){
+    img.forEach(img=>img.classList.toggle('dark'))
+    body.classList.toggle('darkBody')
+    hands.forEach(hand=>hand.classList.toggle('darkHand'))
+    triangle.forEach(triangle=>triangle.classList.toggle('darkTriangle'))
+    AmPm.classList.toggle('darkAmPm')
+    darkToggle.classList.toggle('darkToggle')
+    darkToggleButton.classList.toggle('darkToggleButton')
+    darkContainer.classList.toggle('darkContainer')
+    darkSubmitButton.classList.toggle('darkSubmit')
+    darkInputs.forEach(inputs=>inputs.classList.toggle('darkInputs'))
+    stopButton.classList.toggle('darkSnoozBtn')
+    snoozeButton.classList.toggle('darkSnoozBtn')
+    alarmContainer.classList.toggle('darkContainerText')
+}
+const getTime = function(){
+    const time = new Date()
+    const hours = time.getHours()
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    return [hours,minutes,seconds]
+}
+const clock = function(){
+    const [hours,minutes,seconds] = getTime()
+   
+
+    //Seconds
+    const degSeconds = 6 * seconds
+    secondsHand.style.transform = `rotate(${degSeconds+90}deg)`
+
+    //Minutes
+    const degMinutes = 6 * minutes
+    minutesHand.style.transform = `rotate(${degMinutes+90}deg)`
+
+    //Hours
+    const degHours =  ((hours/12)*360)+((minutes/60)*30)+90
+    hoursHand.style.transform = `rotate(${degHours}deg)`
+
+    //Am - Pm 
+    AmPm.innerText = hours>=12 ? 'PM' : 'AM'
+    
+
+}
+
+const inputData = function(e){
+   e?e.preventDefault():''
+    const inputs = document.querySelectorAll('.alarm_inputs')
+    let [hourst,minutest,secondst] = [...inputs].map(input=> +input.value)
+    hours = hourst
+    seconds = secondst
+    minutes = minutest
+    inputs.forEach(input=>input.innerText='')
+    return [hours,minutes,seconds]
+}
+const calculateAlarmTime = function(e){
+    const [aHours,aMinutes,aSeconds] = getTime()
+    if(hours>24||hours<0||minutes>60||minutes<0||seconds>60||seconds<0) return
+    let [resHour,resMinutes,resSeconds] = [hours+aHours,minutes+aMinutes,seconds+aSeconds]
+    if(resHour>24)resHour-=24
+    if(resMinutes>60){
+        resHour++
+        resMinutes-=60
+    }
+    if(resSeconds>60){
+        resMinutes++
+        resSeconds-=60
+}aHour = resHour
+aMinute = resMinutes
+aSecond = resSeconds
+return [resHour,resMinutes,resSeconds]
+    }
+const hhMmSs = function(seconds){
+    return new Date(seconds*1000).toISOString().slice(11,19)
+}
+const alarm = function(e){
+ const [hours,minutes,seconds] = getTime()
+  totalSec = (aHour*3600)+(aMinute*60)+(aSecond)
+  let timeSec = (hours*3600)+(minutes*60)+(seconds)
+ if(+totalSec === +timeSec){
+    snoozeButton.style.display='inline-block'
+    alarmText.style.display = 'none'
+    stopButton.style.display ='inline-block'
+    audio.play()
+clearInterval(intervalId)
+ }else{
+    snoozeButton.style.display='none'
+    alarmText.style.display = 'inline-block'
+    stopButton.style.display ='none'
+
+    alarmContainer.style.display = 'inline-block'
+ const time = hhMmSs(totalSec-timeSec)
+ alarmText.innerText = time
+}
+console.log(timeSec)
+}
+
+const snooze = function(){
+    stop()
+alarmText.innerText = ''
+aMinute+=5
+intervalId =  setInterval(alarm,1000)
+snoozeButton.style.display = 'none'
+stopButton.style.display = 'none'
+alarmText.style.display = 'inline-block'
+}
+const stop = function(){
+    audio.pause()
+    audio.currentTime = 0
+}
+const stopAlarm = function(e){
+    e.preventDefault()
+    minutes = seconds = hours = aMinute = aHour = aSecond = 0
+    stop()
+    clearInterval(intervalId)
+    alarmText.innerText = ''
+    alarmContainer.style.display = 'none'
+}
+setInterval(clock,1000)
+clock()
+darkSubmitButton.addEventListener('click',(e)=>{
+    inputData(e)
+    calculateAlarmTime(e)
+   intervalId =  setInterval(alarm,1000)
+})
+stopButton.addEventListener('click',stopAlarm)
+snoozeButton.addEventListener('click',snooze)
+
+darkToggle.addEventListener('click',darkMode)
